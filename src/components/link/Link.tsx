6025ce229a79d5eae74877ae0a90acb3ddfc9eb7
Altmertrick <altmertricks@gmail.com>
2023-02-12 14:35:18 +0200

    custom-nav Link fix default behavior on cmd/ctrl pressed

--- src/components/link/Link.tsx
+++ src/components/link/Link.tsx
@@ -1,5 +1,6 @@
 import { useContext } from 'react';
 import NavigationContext from '../../context/navigation';
+import classNames from 'classnames';
 
 //Purpose of Link comp is to prevent normal browser behavior(reloading page)
 //when clicking on <a>
@@ -7,13 +8,21 @@ const Link: React.FC<any> = ({ to, children }) => {
   const { navigate } = useContext(NavigationContext);
 
   const handleClick = (e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
+    //if user holds commandKey (e.metaKey) ro ctrlKey
+    // we shouldn't prevent browser default behavior (open new tab)
+    console.log(e);
+    if (e.altKey || e.metaKey) return;
+
     e.preventDefault();
 
     //updating current path
     navigate(to);
   };
+
+  const classes = classNames('text-blue-500');
+
   return (
-    <a href={to} onClick={handleClick}>
+    <a className={classes} href={to} onClick={handleClick}>
       {children}
     </a>
   );
