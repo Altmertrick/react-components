19d1b7621779ce2fdbee6d0c6ece48b0daf65515
Altmertrick <altmertricks@gmail.com>
2023-02-23 13:36:49 +0200

    displayed 2 dropdowns

--- src/pages/dropdownPage/DropdownPage.tsx
+++ src/pages/dropdownPage/DropdownPage.tsx
@@ -2,22 +2,40 @@ import { useState, useEffect, useRef } from 'react';
 import Dropdown, { OptionT } from '../../components/dropdown/Dropdown';
 
 const DropdownPage = () => {
-  const [value, setValue] = useState<OptionT | null>(null);
+  const [valueColor, setValueColor] = useState<OptionT | null>(null);
+  const [valueDrink, setValueDrink] = useState<OptionT | null>(null);
 
-  const handleSelect = (value: OptionT) => {
-    setValue(value);
+  const handleSelectColors = (value: OptionT) => {
+    setValueColor(value);
+  };
+  const handleSelectDrinks = (value: OptionT) => {
+    setValueDrink(value);
   };
 
-  const options = [
+  const colors = [
     { label: 'Red', value: 'red' },
     { label: 'Green', value: 'green' },
     { label: 'Blue', value: 'blue' },
+  ];
+  const drinks = [
+    { label: 'Tea', value: 'tea' },
+    { label: 'Coffee', value: 'coffee' },
+    { label: 'Milk', value: 'milk' },
   ];
 
   return (
     <div>
       <div className="flex justify-center flex-wrap">
-        <Dropdown options={options} value={value} onChange={handleSelect} />
+        <Dropdown
+          options={colors}
+          value={valueColor}
+          onChange={handleSelectColors}
+        />
+        <Dropdown
+          options={drinks}
+          value={valueDrink}
+          onChange={handleSelectDrinks}
+        />
       </div>
     </div>
   );
